1dec90ab01c05d701cce8454eedb872700d99bc6
djaieh-sketch25 <djaieh@gmail.com>
2026-06-11 14:56:04 +0100

    Update StepsWriteApp.swift

--- StepsWrite/StepsWriteApp.swift
+++ StepsWrite/StepsWriteApp.swift
@@ -4,11 +4,24 @@
 //
 //  Created by Bassem (NextGLabs) on 21/03/2021.
 //
-
 import SwiftUI
+import HealthKit
 
 @main
 struct StepsWriteApp: App {
+    
+    let healthStore = HealthStore()
+    
+    init() {
+        healthStore.requestAuthorization { success, error in
+            if success {
+                print("HealthKit authorization granted")
+            } else {
+                print("HealthKit authorization denied: \(String(describing: error))")
+            }
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
